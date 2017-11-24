00da2782d7f67285369ba84806f659b38025300c
candle <ted@wicrep.com>
2017-11-24 16:02:05 +0900

    If there is no input directory file in the output directory, create a file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import globby from 'globby'
+
 module.exports = babel => {
   var t = babel.types;
 
@@ -6,7 +9,32 @@ module.exports = babel => {
     visitor: {
       Program: {
         enter(path, state) {
+          const { input, output } = state.opts
 
+          if (!input) {
+            throw new Error('require input option')
+          }
+
+          if (!output) {
+            throw new Error('require output option')
+          }
+
+          const inputFiles = globby.sync(input)
+          const outputFiles = globby.sync(output)
+
+          const outputDirPath = output.split("/").reverse().slice(1).reverse().join("/")
+
+          const inputFilesName = inputFiles.map(f =>  f.split("/").pop())
+          const outputFilesName = outputFiles.map(f => f.split("/").pop())
+
+          // If there is no input directory file in the output directory, create a file
+          inputFilesName.map(
+            f => {
+              if (outputFilesName.indexOf(f) == -1){
+                fs.writeFileSync( outputDirPath+"/" + f, "")
+              }
+            }
+          )
         }
       }
     }
